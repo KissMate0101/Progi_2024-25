887734f822c10bbe1ffa3e5955cb10b4e34951c9
RedBall4 <kiss.mate2008@ckik.hu>
2025-01-10 10:37:09 +0100

    Progi repo

--- mester/kozéphaladó/meg melegebb_telepulesek/Program.cs
+++ mester/kozéphaladó/meg melegebb_telepulesek/Program.cs
@@ -12,24 +12,97 @@ namespace meleg_telepulesek
             Beolvas(matrix, out n, out m);
 
             // van e benne 18 nal nagyobb homerseklet?
-           ;
-            Console.WriteLine(Proba(matrix, n, m));
-        }
+
+            //Console.WriteLine(Proba(matrix, n, m));
+
+            //irjuk ki a számpárokat:
+
+            //SzamP(matrix, n, m);
+
+            int j = MasnalMelegebb(matrix, n, m);
+        }    
 
         static void Beolvas(int[,] matrix, out int n, out int m)
         {
-            string[] sor = Console.ReadLine().Split(' ');
+            string[] sor = Console.ReadLine().Split(' '); // {"3", "5"}
             n = Convert.ToInt32(sor[0]);
             m = Convert.ToInt32(sor[1]);
 
             for (int i = 0; i < n; i++)
             {
-                string s = Console.ReadLine();
-                sor = s.Split(' ');
+                string[] s = Console.ReadLine().Split(' ');
                 for (int j = 0; j < m; j++)
                 {
-                    matrix[i, j] = Convert.ToInt32(sor[j]);
+                    matrix[i, j] = Convert.ToInt32(s[j]);
                 }
+            }
+        }
+
+        /*
+        static void SzamP(int[,] matrix, int n, int m)
+        {
+
+            for (int i = 1; i <= 3; i++)
+            {
+                for (int j = i + 1; j <= 4; j++)
+                {
+                    Console.Write($"({i}, {j})");
+                }
+            }
+        }
+        */
+
+        static void SzamP(int[,] matrix, int n, int m)
+        {
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"{i} {j} {Melegebb(matrix, n, i, j)} \t");
+                }
+            }
+        }
+
+        private static int MasnalMelegebb(int[,] matrix, int n, int m)
+        {
+            int i = 0;
+            int j = 0;
+            bool megvanE = false;
+            while (i < n && !(megvanE))
+            {
+                j = 0;
+                while (i < n && !(megvanE))
+                {
+                    megvanE = Melegebb(matrix, m, i, j);
+                    j++;
+                }
+                i++;
+            }
+            if (megvanE)
+            {
+                return i;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        static bool Melegebb(int[,] matrix, int m, int i, int j)
+        {
+            int k = 0;
+            while (k < m && !(matrix[i, k] > matrix[j, k]))
+            {
+                k++;
+            }
+            if (i >= m)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
 
@@ -51,8 +124,6 @@ namespace meleg_telepulesek
                 }
                 i++;
             }
-
-            Console.WriteLine($"{i}{j}");
             if (megvanE)
             {
                 return matrix[i - 1, j - 1];
