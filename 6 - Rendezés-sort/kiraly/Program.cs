2264f71f45eab171f60df1c4fd556279c906c296
RedBall4 <kiss.mate2008@ckik.hu>
2025-02-26 12:49:08 +0100

    fajlolvasás

---
+++ 6 - Rendezés-sort/kiraly/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kiraly
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> nevek = new List<string>();
+            List<int> evszam = new List<int>();
+            List<int> hossz = new List<int>();
+            //Beolvas1(nevek, primek);
+            //Beolvas2(nevek, primek);
+            Beolvas(nevek, evszam, hossz);
+
+            List<int> idosav = new List<int>();
+            Rendezes(nevek, evszam, hossz);
+            Kiir(nevek, evszam, hossz);
+            //Hozzafuz(nevek, primek);
+        }
+
+        static void Kiir(List<string> nevek, List<int> evszam, List<int> hossz)
+        {
+            for (int i = 0; i < nevek.Count; i++)
+            {
+                Console.WriteLine($"{nevek[i]}, {evszam[i]}, {hossz[i]}");
+            }
+        }
+
+        static void Rendezes(List<string> nevek, List<int> evszam, List<int> hossz)
+        {
+            for (int i = 0; i < hossz.Count; i++)
+            {
+                int k = i;
+                for(int j = k; j < hossz.Count; j++)
+                {
+                    if (hossz[j] > hossz[k] || hossz[j] == hossz[k] && hossz[j] < evszam[k])
+                    {
+                        k = j;
+                    }
+                }
+                Csere(nevek, i, k);
+                Csere(evszam, i, k);
+                Csere(hossz, i, k);
+            }
+        }
+
+         static void Csere<T>(List<T> idosav, int i, int j)
+        {
+            (idosav[j], idosav[i]) = (idosav[i], idosav[j]);
+        }
+
+        static void Beolvas(List<string> nevek, List<int> evszam, List<int> hossz)
+        {
+            StreamReader reader = new StreamReader("adatok.txt");
+            while (!reader.EndOfStream)
+            {
+                string[] sor = reader.ReadLine().Split(',');
+                nevek.Add(sor[0]);
+                evszam.Add(int.Parse(sor[1]));
+                hossz.Add(int.Parse(sor[2]));
+            }
+            reader.Close();
+        }
+
+    }
+}
---
+++ 6 - Rendezés-sort/kiraly/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("kiraly")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("kiraly")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("db231281-18ef-477b-b5ea-8f61c430e866")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
